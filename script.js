23d1ca905357a796b106eadf5d691c6d2002ff1e
belunatic <abelluvyou@yahoo.com>
2025-09-23 17:23:22 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+const userNameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirmPassword");
