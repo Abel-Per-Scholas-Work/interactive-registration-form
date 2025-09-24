145cbe61caacfdaa84bd9a8c1b08b877bc17fec1
belunatic <abelluvyou@yahoo.com>
2025-09-23 17:46:45 -0700

    validate the username not to be none-empty

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+//form
 const form = document.querySelector("form");
 const userNameInput = document.querySelector("#username");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const confirmPasswordInput = document.querySelector("#confirmPassword");
+//error messages spn
+const usernameError = document.querySelector("#usernameError");
+const emailError = document.querySelector("#emailError");
+const passwordError = document.querySelector("#passwordError");
+const passwordConfirmError = document.querySelector("#usernameError");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	console.log("submitted");
+});
+
+//username Input
+userNameInput.addEventListener("input", (e) => {
+	if (userNameInput.value === "") {
+		userNameInput.setCustomValidity("Username cannot be empty");
+		console.log("not valid");
+	} else {
+		userNameInput.setCustomValidity("");
+		console.log("valid");
+	}
+	usernameError.textContent = userNameInput.validationMessage;
+});
